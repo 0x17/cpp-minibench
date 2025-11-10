1e7b9cd1b192b6d6f8e80cd4c9a5770718b0431a
André Schnabel <aschnabel@gams.com>
2025-11-10 08:03:12 +0100

    Use own tolower function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@
 
 using namespace std::literals::string_literals;
 
+inline char tolower( const char c )
+{
+    return c >= 'A' && c <= 'Z' ? static_cast<char>( c ^ 32 ) : c;
+}
+
 static bool SameTextStdEqual(const std::string_view a,
                              const std::string_view b)
 {
@@ -15,7 +20,7 @@ static bool SameTextStdEqual(const std::string_view a,
     return std::equal(a.begin(), a.end(), b.begin(),
                       [](const unsigned char c1, const unsigned char c2)
                       {
-                          return c1 == c2 || std::tolower(c1) == std::tolower(c2);
+                          return c1 == c2 || tolower(c1) == tolower(c2);
                       });
 }
 
@@ -24,7 +29,7 @@ static bool SameTextForLoop(const std::string_view S1, const std::string_view S2
     if (S1.length() != S2.length()) return false;
     for (size_t i{}; i < S1.length(); i++)
     {
-        if (S1[i] != S2[i] && std::tolower(S1[i]) != std::tolower(S2[i]))
+        if (S1[i] != S2[i] && tolower(S1[i]) != tolower(S2[i]))
             return false;
     }
     return true;
